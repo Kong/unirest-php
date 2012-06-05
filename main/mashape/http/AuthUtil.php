d11faed835d73c73ba8b18fd2c9c9a8ee2f1beb1
Marco Palladino <marco@mashape.com>
2012-06-05 16:51:25 +0200

    auth

--- main/mashape/http/AuthUtil.php
+++ main/mashape/http/AuthUtil.php
@@ -29,9 +29,8 @@ class AuthUtil {
 	public static function generateAuthenticationHeader($publicKey, $privateKey) {
 		$header = "";
 		if (!($publicKey == null || $privateKey == null)) {
-			$uuid = uniqid();
-			$hash = hash_hmac("sha1", $uuid, $privateKey);
-			$header = "X-Mashape-Authorization: " . base64_encode($publicKey . ":" . $hash . $uuid) . "\r\n";
+			$hash = hash_hmac("sha1", $publicKey, $privateKey);
+			$header = "Proxy-Authorization: " . base64_encode($publicKey . ":" . $hash) . "\r\n";
 		}
 		return $header;
 	}
