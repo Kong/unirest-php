28102207a257bc3251f97c48863bae55744a70df
thefosk <marco@mashape.com>
2013-02-14 16:13:44 -0800

    version 3.0

--- main/mashape/authentication/Authentication.php
+++
@@ -1,7 +0,0 @@
-<?php
-interface Authentication {
-	public function handleHeaders();
-
-	public function handleParams();
-}
-?>
--- main/mashape/authentication/HeaderAuthentication.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once(dirname(__FILE__) . "/Authentication.php");
-
-abstract class HeaderAuthentication implements Authentication {
-	public function handleParams() {
-		return null;
-	}
-}
-?>
