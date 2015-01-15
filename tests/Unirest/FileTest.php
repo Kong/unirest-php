980c204c414ec092c68fe585080ff8ea392135ee
Ahmad Nassri <ahmad@codeinchaos.com>
2015-01-14 20:01:38 -0500

    PHP 5.4 test

--- tests/Unirest/FileTest.php
+++ tests/Unirest/FileTest.php
@@ -7,6 +7,11 @@ class UnirestFileTest extends \PHPUnit_Framework_TestCase
     public function testCURLFile()
     {
         $file = File::add(UPLOAD_FIXTURE);
-        $this->assertTrue($file instanceof \CURLFile);
+
+        if (PHP_MAJOR_VERSION === 5 && PHP_MINOR_VERSION === 4) {
+            $this->assertEquals($file, sprintf('@%s;filename=%s;type=', UPLOAD_FIXTURE, basename(UPLOAD_FIXTURE)));
+        } else {
+            $this->assertTrue($file instanceof \CURLFile);
+        }
     }
 }
