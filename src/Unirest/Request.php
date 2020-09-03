b8f7c5f7486cbf66bf39c69d14d829dbf92506ba
Grégoire <58863594+Warbufl@users.noreply.github.com>
2020-09-03 11:17:01 +0200

    Update Request.php
    Hello for php 7.4 for correct Notice: Trying to access array offset on value int.

--- src/Unirest/Request.php
+++ src/Unirest/Request.php
@@ -555,7 +555,7 @@ class Request
             $query = '?' . http_build_query(self::getArrayFromQuerystring($query));
         }
 
-        if ($port && $port[0] !== ':') {
+        if ($port && strval($port)[0] !== ':') {
             $port = ':' . $port;
         }
 
